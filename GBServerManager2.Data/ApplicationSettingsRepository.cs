558e661e5417a5a4c8da6b5e94923a721cd7d2aa
zeiktuvai <zeik.tuvaii@gmail.com>
2023-06-27 20:34:46 -0400

    Added app settings and game server repo

---
+++ GBServerManager2.Data/ApplicationSettingsRepository.cs
@@ -0,0 +1,31 @@
+﻿using GBServerManager2.Models.Options;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBServerManager2.Data
+{
+    public class ApplicationSettingsRepository
+    {
+        private LiteDatabase _db;
+
+        public ApplicationSettingsRepository(LiteDbContext dbContext)
+        {
+            _db = dbContext.Database;
+        }
+
+        public IEnumerable<AppSetting> GetApplicationSettings()
+        {
+            var result = _db.GetCollection<AppSetting>("ApplicationSettings").FindAll();
+            return result;
+        }
+
+        public int saveApplicationSetting(AppSetting setting)
+        {
+            return _db.GetCollection<AppSetting>("ApplicationSettings").Insert(setting);
+        }
+    }
+}
---
+++ GBServerManager2.Data/GameServerRepository.cs
@@ -0,0 +1,21 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBServerManager2.Data
+{
+    public class GameServerRepository
+    {
+        private LiteDatabase _db;
+
+        public GameServerRepository(LiteDbContext dbContext)
+        {
+            _db = dbContext.Database;
+        }
+
+
+    }
+}
