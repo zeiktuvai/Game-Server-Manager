d2face8bcb06c2c59fa3277b71dea9ea939ac3d7
zeiktuvai <zeik.tuvaii@gmail.com>
2023-07-02 17:22:52 -0400

    Ui work on the settings screen

---
+++ GBServerManager2.Services/Helpers/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBServerManager2.Services.Helpers
+{
+    public static class FileHelper
+    {
+        public static bool CheckDirectoryExists(string path)
+        {
+            return Directory.Exists(path);
+        }
+    }
+}
