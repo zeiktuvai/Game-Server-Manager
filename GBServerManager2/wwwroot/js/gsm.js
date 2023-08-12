0572e38f883449e38793bad33fdb826fb352e7f0
zeiktuvai <zeik.tuvaii@gmail.com>
2023-08-11 22:03:53 -0400

    Work on process console dialog.

--- GBServerManager2/wwwroot/js/gsm.js
+++ GBServerManager2/wwwroot/js/gsm.js
@@ -2,3 +2,8 @@
     textarea.scrollTop = textarea.scrollHeight;
     textarea.scrollToEnd();
 }
+
+function scrollEnd(elementName) {
+    var element = document.getElementById(elementName);
+    element.scrollTop = element.scrollHeight;
+}
